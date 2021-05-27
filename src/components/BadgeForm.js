7512afb15a116fa5f7a34f35a378339a570ca37f
jara77 <rafaeljara73@gmail.com>
2021-05-27 13:11:09 -0700

    First Commit

---
+++ src/components/BadgeForm.js
@@ -0,0 +1,89 @@
+import React from "react";
+
+class BadgeForm extends React.Component {
+  handleClick = (e) => {
+    console.log("Button was clicked");
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form was Sumbit");
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>New Attendant</h1>
+
+        <form onSubmit={this.handleSubmit} action="">
+          <div className="form-group">
+            <label>First Name</label>
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="firstName"
+              placeholder="First Name"
+              value={this.props.formValues.firstName}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Last Name</label>
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="lastName"
+              placeholder="Last Name"
+              value={this.props.formValues.lastName}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Email</label>
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="email"
+              placeholder="Email"
+              value={this.props.formValues.email}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Job Tittle</label>
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="jobTitle"
+              placeholder="Job Title"
+              value={this.props.formValues.jobTitle}
+            />
+          </div>
+
+          <div className="form-group">
+            <label>Twitter</label>
+            <input
+              onChange={this.props.onChange}
+              className="form-control"
+              type="text"
+              name="twitter"
+              placeholder="Twitter"
+              value={this.props.formValues.twitter}
+            />
+          </div>
+
+          <button onClick={this.handleClick} className="btn btn-primary">
+            Save
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default BadgeForm;
